4813af1414774b5923e741639fa272dc8ff0cdf3
Hironobuizumi <158254076+Hironobuizumi@users.noreply.github.com>
2024-04-15 14:24:58 +0900

    010

---
+++ kadai_010/summary.js
@@ -0,0 +1,13 @@
+$('#change-color').click(function() {
+    $('#target').css('color', 'blue'); 
+  });
+  $('#change-text').click(function() {
+    $('#target').text('こんばんは！'); 
+  });
+  $('#fade-out').click(function() {
+    $('#target').fadeOut('slow'); 
+  });
+  $('#fade-in').click(function() {
+    $('#target').fadeIn('slow'); 
+  });
+        
